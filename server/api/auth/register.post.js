659b0dd27aa99feb5f500f60f860587334fc69a3
MortezaSoltanpour <morteza.cs@gmail.com>
2024-07-20 16:08:25 +0330

    Set cookie

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -10,6 +10,13 @@ export default defineEventHandler(async (event) => {
       body,
     });
 
+    setCookie(event, "data", data.flat(), {
+      httpOnly: true,
+      secure: true,
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+      path: "/",
+    });
+
     return data;
   } catch (error) {
     return error;
