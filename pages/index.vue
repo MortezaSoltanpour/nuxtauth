acfb74ce63364ac4d81668b8c9b48f323fd50bd8
MortezaSoltanpour <morteza.cs@gmail.com>
2024-07-20 13:47:14 +0330

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-body text-center">
+        <h1 class="mb-4">Morteza</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, qui
+          ullam rerum harum placeat est quas maiores beatae, aliquid nesciunt et
+          assumenda quod dolore nobis corrupti necessitatibus! Consequuntur,
+          magnam dolorem?
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
